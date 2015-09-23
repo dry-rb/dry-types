63d23ec5fc0a7f92eb85887992f4df0fa571313d
Piotr Solnica <piotr.solnica@gmail.com>
2015-09-23 13:27:50 +0200

    Some benchmarks, people like benchmarks

--- Gemfile
+++ Gemfile
@@ -5,4 +5,7 @@ gemspec
 group :tools do
   gem 'benchmark-ips'
   gem 'byebug'
+  gem 'virtus'
+  gem 'fast_attributes'
+  gem 'attrio'
 end
---
+++ benchmarks/basic.rb
@@ -0,0 +1,59 @@
+require 'dry/data'
+require 'virtus'
+require 'fast_attributes'
+require 'attrio'
+require 'ostruct'
+
+require 'benchmark/ips'
+
+class VirtusUser
+  include Virtus.model
+
+  attribute :name, String
+  attribute :age, Integer
+end
+
+class FastUser
+  extend FastAttributes
+
+  define_attributes initialize: true, attributes: true do
+    attribute :name, String
+    attribute :age,  Integer
+  end
+end
+
+class AttrioUser
+  include Attrio
+
+  define_attributes do
+    attr :name, String
+    attr :age, Integer
+  end
+
+  def initialize(attributes = {})
+    self.attributes = attributes
+  end
+
+  def attributes=(attributes = {})
+    attributes.each do |attr,value|
+      self.send("#{attr}=", value) if self.respond_to?("#{attr}=")
+    end
+  end
+end
+
+class DryDataUser
+  include Dry::Data::Struct
+
+  attributes(name: String, age: Integer)
+end
+
+puts DryDataUser.new(name: 'Jane', age: '21').inspect
+
+Benchmark.ips do |x|
+  x.report('virtus') { VirtusUser.new(name: 'Jane', age: '21') }
+  x.report('fast_attributes') { DryDataUser.new(name: 'Jane', age: '21') }
+  x.report('attrio') { AttrioUser.new(name: 'Jane', age: '21') }
+  x.report('dry-data') { FastUser.new(name: 'Jane', age: '21') }
+
+  x.compare!
+end
