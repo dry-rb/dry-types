fba343eefbb61c80b53d13b28338739b78091f56
Piotr Solnica <piotr.solnica@gmail.com>
2015-10-05 14:33:33 +0200

    Restructure hash spec

--- spec/dry/data/type/hash_spec.rb
+++ spec/dry/data/type/hash_spec.rb
@@ -1,24 +1,32 @@
 RSpec.describe Dry::Data::Type::Hash do
-  describe '#schema' do
-    it 'builds hash using provided schema' do
-      phone = Struct.new(:prefix, :number) do
-        def self.name
-          'Phone'
-        end
+  subject(:hash) do
+    Dry::Data['coercible.hash'].schema(
+      name: "coercible.string",
+      age: "coercible.int",
+      active: "strict.bool",
+      phone: "phone"
+    )
+  end
+
+  let(:phone) do
+    Dry::Data['phone'].primitive
+  end
+
+  before :all do
+    phone = Struct.new(:prefix, :number) do
+      def self.name
+        'Phone'
       end
+    end
 
-      Dry::Data.register(
-        "phone",
-        Dry::Data::Type.new(-> args { phone.new(*args) }, phone)
-      )
+    Dry::Data.register(
+      "phone",
+      Dry::Data::Type.new(-> args { phone.new(*args) }, phone)
+    )
+  end
 
-      hash = Dry::Data['coercible.hash'].schema(
-        name: "coercible.string",
-        age: "coercible.int",
-        active: "strict.bool",
-        phone: "phone"
-      )
-
+  describe '#[]' do
+    it 'builds hash using provided schema' do
       user_hash = hash[
         name: :Jane, age: '21', active: true, phone: ['+48', '123-456-789']
       ]
@@ -26,7 +34,9 @@ RSpec.describe Dry::Data::Type::Hash do
       expect(user_hash).to eql(
         name: 'Jane', age: 21, active: true, phone: phone.new('+48', '123-456-789')
       )
+    end
 
+    it 'raises SchemaError if constructing one of the values raised an error' do
       expect {
         hash[name: 'Jane', age: 21, active: 'true', phone: nil]
       }.to raise_error(
