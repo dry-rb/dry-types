864a1966abc80e4a06772d0bca527cf8b4f7a9bb
John Backus <johncbackus@gmail.com>
2016-04-03 02:51:34 -0700

    Move code shared in Form and JSON into Coercions

--- lib/dry/types/coercions/form.rb
+++ lib/dry/types/coercions/form.rb
@@ -8,23 +8,8 @@ module Dry
         TRUE_VALUES = %w[1 on  t true  y yes].freeze
         FALSE_VALUES = %w[0 off f false n no].freeze
         BOOLEAN_MAP = ::Hash[TRUE_VALUES.product([true]) + FALSE_VALUES.product([false])].freeze
-        EMPTY_STRING = ''.freeze
 
-        def self.to_nil(input)
-          JSON.to_nil(input)
-        end
-
-        def self.to_date(input)
-          JSON.to_date(input)
-        end
-
-        def self.to_date_time(input)
-          JSON.to_date_time(input)
-        end
-
-        def self.to_time(input)
-          JSON.to_time(input)
-        end
+        extend Coercions
 
         def self.to_true(input)
           BOOLEAN_MAP.fetch(input, input)
@@ -67,11 +52,6 @@ module Dry
             result
           end
         end
-
-        def self.empty_str?(value)
-          EMPTY_STRING.eql?(value)
-        end
-        private_class_method :empty_str?
       end
     end
   end
--- lib/dry/types/coercions/json.rb
+++ lib/dry/types/coercions/json.rb
@@ -7,27 +7,7 @@ module Dry
   module Types
     module Coercions
       module JSON
-        def self.to_nil(input)
-          input unless ''.eql?(input)
-        end
-
-        def self.to_date(input)
-          Date.parse(input)
-        rescue ArgumentError
-          input
-        end
-
-        def self.to_date_time(input)
-          DateTime.parse(input)
-        rescue ArgumentError
-          input
-        end
-
-        def self.to_time(input)
-          Time.parse(input)
-        rescue ArgumentError
-          input
-        end
+        extend Coercions
 
         def self.to_decimal(input)
           if input.is_a?(String) && input == ''
