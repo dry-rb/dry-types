9b0302fa0b04de2ce3054de9b5617483427eedb7
John Backus <johncbackus@gmail.com>
2016-04-03 15:34:02 -0700

    Add tests for `Result` and `Definition`
    - Brings mutation coverage of Result* up to 100% from 58.33%
    - Specifies behavior of Definition#try
    
    Result::Success and Result::Failure seem simple enough to assume that
    they are implementation details so I tested the behavior through
    Definition#try

--- spec/dry/types/definition_spec.rb
+++ spec/dry/types/definition_spec.rb
@@ -14,4 +14,36 @@ RSpec.describe Dry::Types::Definition do
       expect(coercible_string[{}]).to eql({}.to_s)
     end
   end
+
+  describe '#try' do
+    subject(:try) { type.try(value) }
+
+    let(:success) { try.success?    }
+    let(:failure) { try.failure?    }
+    let(:input)   { try.input       }
+
+    context 'when given valid input' do
+      let(:value) { 'foo' }
+
+      specify { expect(success).to be(true)  }
+      specify { expect(failure).to be(false) }
+      specify { expect(input).to be(value)   }
+    end
+
+    context 'when given invalid input' do
+      let(:value) { :foo }
+
+      specify { expect(success).to be(false) }
+      specify { expect(failure).to be(true)  }
+      specify { expect(input).to be(value)   }
+
+      it "provides an error message" do
+        expect(try.error).to eql(':foo must be an instance of String')
+      end
+
+      it "yields failure when given a block" do
+        expect { |probe| type.try(value, &probe) }.to yield_with_args(try)
+      end
+    end
+  end
 end
