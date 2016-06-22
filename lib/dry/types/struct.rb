5108b550d2b12087dacf941eff0fbb2721719563
Nikita Shilnikov <ns@latera.ru>
2016-06-22 23:22:50 +0300

    Make Struct work as a type definition
    fixes #81

--- lib/dry/types/struct.rb
+++ lib/dry/types/struct.rb
@@ -1,84 +1,10 @@
 require_relative 'hashify'
+require 'dry/types/struct/class_interface'
 
 module Dry
   module Types
     class Struct
-      class << self
-        attr_reader :constructor
-      end
-
-      def self.inherited(klass)
-        super
-
-        klass.instance_variable_set('@equalizer', Equalizer.new(*schema.keys))
-        klass.instance_variable_set('@constructor_type', constructor_type)
-        klass.send(:include, klass.equalizer)
-
-        unless klass == Value
-          klass.instance_variable_set('@constructor', Types['coercible.hash'])
-          Types.register_class(klass)
-        end
-
-        klass.attributes({}) unless equal?(Struct)
-      end
-
-      def self.equalizer
-        @equalizer
-      end
-
-      def self.attribute(name, type)
-        attributes(name => type)
-      end
-
-      def self.attributes(new_schema)
-        check_schema_duplication(new_schema)
-
-        prev_schema = schema
-
-        @schema = prev_schema.merge(new_schema)
-        @constructor = Types['coercible.hash'].public_send(constructor_type, schema)
-
-        attr_reader(*new_schema.keys)
-        equalizer.instance_variable_get('@keys').concat(new_schema.keys)
-
-        self
-      end
-
-      def self.check_schema_duplication(new_schema)
-        shared_keys = new_schema.keys & schema.keys
-
-        fail RepeatedAttributeError, shared_keys.first if shared_keys.any?
-      end
-      private_class_method :check_schema_duplication
-
-      def self.constructor_type(type = nil)
-        if type
-          @constructor_type = type
-        else
-          @constructor_type || :strict
-        end
-      end
-
-      def self.schema
-        super_schema = superclass.respond_to?(:schema) ? superclass.schema : {}
-        super_schema.merge(@schema || {})
-      end
-
-      def self.new(attributes = default_attributes)
-        if attributes.instance_of?(self)
-          attributes
-        else
-          super(constructor[attributes])
-        end
-      rescue SchemaError, SchemaKeyError => error
-        raise StructError, "[#{self}.new] #{error}"
-      end
-
-      def self.default_attributes
-        schema.each_with_object({}) { |(name, type), result|
-          result[name] = type.default? ? type.evaluate : type[nil]
-        }
-      end
+      extend ClassInterface
 
       def initialize(attributes)
         attributes.each { |key, value| instance_variable_set("@#{key}", value) }
--- spec/shared/struct.rb
+++ spec/shared/struct.rb
@@ -1,17 +1,12 @@
 RSpec.shared_examples_for Dry::Types::Struct do
+  let(:jane) { { name: :Jane, age: '21', root: true, address: { city: 'NYC', zipcode: 123 } } }
+  let(:mike) { { name: :Mike, age: '43', root: false, address: { city: 'Atlantis', zipcode: 456 } } }
+  let(:john) { { name: :John, age: '36', root: false, address: { city: 'San Francisco', zipcode: 789 } } }
+
   describe '#eql' do
     context 'when struct values are equal' do
-      let(:user_1) do
-        type[
-          name: :Jane, age: '21', root: true, address: { city: 'NYC', zipcode: 123 }
-        ]
-      end
-
-      let(:user_2) do
-        type[
-          name: :Jane, age: '21', root: true, address: { city: 'NYC', zipcode: 123 }
-        ]
-      end
+      let(:user_1) { type[jane] }
+      let(:user_2) { type[jane] }
 
       it 'returns true' do
         expect(user_1).to eql(user_2)
@@ -19,17 +14,8 @@ RSpec.shared_examples_for Dry::Types::Struct do
     end
 
     context 'when struct values are not equal' do
-      let(:user_1) do
-        type[
-          name: :Jane, age: '21', root: true, address: { city: 'NYC', zipcode: 123 }
-        ]
-      end
-
-      let(:user_2) do
-        type[
-          name: :Mike, age: '43', root: false, address: { city: 'Atlantis', zipcode: 456 }
-        ]
-      end
+      let(:user_1) { type[jane] }
+      let(:user_2) { type[mike] }
 
       it 'returns false' do
         expect(user_1).to_not eql(user_2)
@@ -39,17 +25,8 @@ RSpec.shared_examples_for Dry::Types::Struct do
 
   describe '#hash' do
     context 'when struct values are equal' do
-      let(:user_1) do
-        type[
-          name: :Jane, age: '21', root: true, address: { city: 'NYC', zipcode: 123 }
-        ]
-      end
-
-      let(:user_2) do
-        type[
-          name: :Jane, age: '21', root: true, address: { city: 'NYC', zipcode: 123 }
-        ]
-      end
+      let(:user_1) { type[jane] }
+      let(:user_2) { type[jane] }
 
       it 'the hashes are equal' do
         expect(user_1.hash).to eql(user_2.hash)
@@ -57,17 +34,8 @@ RSpec.shared_examples_for Dry::Types::Struct do
     end
 
     context 'when struct values are not equal' do
-      let(:user_1) do
-        type[
-          name: :Jane, age: '21', root: true, address: { city: 'NYC', zipcode: 123 }
-        ]
-      end
-
-      let(:user_2) do
-        type[
-          name: :Mike, age: '43', root: false, address: { city: 'Atlantis', zipcode: 456 }
-        ]
-      end
+      let(:user_1) { type[jane] }
+      let(:user_2) { type[mike] }
 
       it 'the hashes are not equal' do
         expect(user_1.hash).to_not eql(user_2.hash)
@@ -76,16 +44,68 @@ RSpec.shared_examples_for Dry::Types::Struct do
   end
 
   describe '#inspect' do
-    let(:user_1) do
-      type[
-        name: :Jane, age: '21', root: true, address: { city: 'NYC', zipcode: 123 }
-      ]
-    end
+    let(:user_1) { type[jane] }
 
     it 'lists attributes' do
       expect(user_1.inspect).to eql(
-        %Q(#<#{type.primitive} name="Jane" age=21 address=#<Test::Address city="NYC" zipcode="123"> root=true>)
+        %Q(#<#{type} name="Jane" age=21 address=#<Test::Address city="NYC" zipcode="123"> root=true>)
       )
     end
+  end
+
+  context 'class interface' do
+    describe '.|' do
+      let(:sum_type) { type_or_nil = type | Dry::Types['strict.nil'] }
+
+      it 'returns Sum type' do
+        expect(sum_type).to be_instance_of(Dry::Types::Sum)
+        expect(sum_type[nil]).to be_nil
+        expect(sum_type[jane]).to eql(type[jane])
+      end
+    end
+
+    describe '.maybe?' do
+      it 'is not a maybe' do
+        expect(type).not_to be_maybe
+      end
+    end
+
+    describe '.default?' do
+      it 'is not a default' do
+        expect(type).not_to be_default
+      end
+    end
+
+    describe '.default' do
+      let(:default_type) { type.default(type[jane]) }
+
+      it 'retruns Default type' do
+        expect(default_type).to be_instance_of(Dry::Types::Default)
+        expect(default_type[nil]).to eql(type[jane])
+      end
+    end
+
+    describe '.enum' do
+      let(:enum_type) { type.enum(type[jane], type[mike]) }
+
+      it 'returns Enum type' do
+        expect(enum_type[type[jane]]).to eql(type[jane])
+        expect { enum_type[type[john]] }.to raise_error(Dry::Types::ConstraintError)
+      end
+    end
+
+    describe '.optional' do
+      let(:optional_type) { type.optional }
+
+      it 'returns Sum type' do
+        expect(optional_type).to eql(Dry::Types['strict.nil'] | type)
+        expect(optional_type[nil]).to be_nil
+        expect(optional_type[jane]).to eql(type[jane])
+      end
+    end
+  end
+
+  it 'registered without wrapping' do
+    expect(Dry::Types[type]).to be type
   end
 end
