6883e47de28bfd104ce027fc37f0ac29105f4391
Piotr Solnica <piotr.solnica@gmail.com>
2016-07-01 11:53:50 +0200

    Remove custom predicates mod (dry-logic now has type? predicate)

--- lib/dry/types/constraints.rb
+++ lib/dry/types/constraints.rb
@@ -3,22 +3,14 @@ require 'dry/logic/predicates'
 
 module Dry
   module Types
-    module Predicates
-      include Logic::Predicates
-
-      predicate(:type?) do |type, value|
-        value.kind_of?(type)
-      end
-    end
-
     def self.Rule(options)
       rule_compiler.(
-        options.map { |key, val| [:val, Predicates[:"#{key}?"].curry(val).to_ast] }
+        options.map { |key, val| [:val, Logic::Predicates[:"#{key}?"].curry(val).to_ast] }
       ).reduce(:and)
     end
 
     def self.rule_compiler
-      @rule_compiler ||= Logic::RuleCompiler.new(Types::Predicates)
+      @rule_compiler ||= Logic::RuleCompiler.new(Logic::Predicates)
     end
   end
 end
