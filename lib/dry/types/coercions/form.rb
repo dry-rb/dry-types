831b9f81d668b1dbdaa2faf8325ab42491d26cc7
Kir Shatrov <shatrov@me.com>
2016-08-05 15:59:03 -0400

    Typecast 'T' and 'F' to boolean values

--- lib/dry/types/coercions/form.rb
+++ lib/dry/types/coercions/form.rb
@@ -5,8 +5,8 @@ module Dry
   module Types
     module Coercions
       module Form
-        TRUE_VALUES = %w[1 on On ON t true True TRUE  y yes Yes YES].freeze
-        FALSE_VALUES = %w[0 off Off OFF f false False FALSE n no No NO].freeze
+        TRUE_VALUES = %w[1 on On ON t true True TRUE T y yes Yes YES].freeze
+        FALSE_VALUES = %w[0 off Off OFF f false False FALSE F n no No NO].freeze
         BOOLEAN_MAP = ::Hash[TRUE_VALUES.product([true]) + FALSE_VALUES.product([false])].freeze
 
         extend Coercions
