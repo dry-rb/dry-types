602dc93aa60bf0c41bb09482c0679cc3074b6338
John Backus <johncbackus@gmail.com>
2016-08-28 16:24:51 -0700

    Add unknown keys error spec for strict schema

--- spec/dry/types/hash_spec.rb
+++ spec/dry/types/hash_spec.rb
@@ -139,5 +139,14 @@ RSpec.describe Dry::Types::Hash do
     include_examples 'hash schema behavior'
     include_examples 'strict schema behavior for missing keys'
     include_examples 'strict typing behavior'
+
+    it 'rejects unexpected keys' do
+      expected_input = { name: :Jane, age: 21, active: true, phone: ['1', '2'] }
+      unexpected_input = { gender: 'F', email: 'Jane@hotmail.biz' }
+
+      expect { hash.call(expected_input.merge(unexpected_input)) }
+        .to raise_error(Dry::Types::UnknownKeysError)
+        .with_message('unexpected keys [:gender, :email] in Hash input')
+    end
   end
 end
