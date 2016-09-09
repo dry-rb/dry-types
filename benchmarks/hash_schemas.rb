03bf9dfc84550ef86d78ad1c5dfb837dcb8fd22d
John Backus <johncbackus@gmail.com>
2016-09-09 16:31:51 -0700

    Add hash schema bench
        Warming up --------------------------------------
                 schema#call    14.277k i/100ms
                   weak#call     7.484k i/100ms
             permissive#call    14.571k i/100ms
                 strict#call    11.650k i/100ms
        strict_with_defaults#call
                                11.306k i/100ms
             symbolized#call     7.448k i/100ms
                  schema#try     6.647k i/100ms
                    weak#try     6.225k i/100ms
              permissive#try     6.553k i/100ms
                  strict#try     5.685k i/100ms
        strict_with_defaults#try
                                 5.734k i/100ms
              symbolized#try     6.598k i/100ms
        Calculating -------------------------------------
                 schema#call    159.320k (± 7.3%) i/s -    799.512k
                   weak#call     81.056k (± 5.5%) i/s -    404.136k
             permissive#call    159.540k (± 6.0%) i/s -    801.405k
                 strict#call    118.076k (±12.8%) i/s -    582.500k
        strict_with_defaults#call
                                113.061k (±13.3%) i/s -    553.994k
             symbolized#call     85.512k (± 6.0%) i/s -    431.984k
                  schema#try     79.341k (± 6.3%) i/s -    398.820k
                    weak#try     73.009k (± 7.8%) i/s -    367.275k
              permissive#try     78.121k (± 8.0%) i/s -    393.180k
                  strict#try     72.733k (± 3.2%) i/s -    363.840k
        strict_with_defaults#try
                                 71.999k (± 4.2%) i/s -    361.242k
              symbolized#try     74.826k (± 8.1%) i/s -    376.086k

---
+++ benchmarks/hash_schemas.rb
@@ -0,0 +1,49 @@
+$LOAD_PATH.unshift('lib')
+
+require 'bundler/setup'
+require 'dry-types'
+
+module SchemaBench
+  def self.hash_schema(type)
+    Dry::Types['hash'].public_send(type,
+      email:   Dry::Types['string'],
+      age:     Dry::Types['form.int'],
+      admin:   Dry::Types['form.bool'],
+      address: Dry::Types['hash'].public_send(type,
+        city: Dry::Types['string'],
+        street: Dry::Types['string']
+      )
+    )
+  end
+
+  private_class_method(:hash_schema)
+
+  SCHEMAS =
+    Dry::Types::Hash
+      .public_instance_methods(false)
+      .map { |schema_type| [schema_type, hash_schema(schema_type)] }
+      .to_h
+
+  INPUT = {
+    email: 'jane@doe.org',
+    age: '20',
+    admin: '1',
+    address: { city: 'NYC', street: 'Street 1/2' }
+  }
+end
+
+require 'benchmark/ips'
+
+Benchmark.ips do |x|
+  SchemaBench::SCHEMAS.each do |schema_type, schema|
+    x.report("#{schema_type}#call") do
+      schema.call(SchemaBench::INPUT)
+    end
+  end
+
+  SchemaBench::SCHEMAS.each do |schema_type, schema|
+    x.report("#{schema_type}#try") do
+      schema.try(SchemaBench::INPUT)
+    end
+  end
+end
