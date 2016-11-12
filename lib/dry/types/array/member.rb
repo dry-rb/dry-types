987e6f25ab5351cc69dc06bf5b65591f2d630f60
Alex Semyonov <alex@semyonov.us>
2016-11-13 00:02:18 +0300

    Document Dry::Types::Array::Member
    Signed-off-by: Alex Semyonov <alex@semyonov.us>

--- lib/dry/types/array/member.rb
+++ lib/dry/types/array/member.rb
@@ -2,22 +2,36 @@ module Dry
   module Types
     class Array < Definition
       class Member < Array
+        # @return [Definition]
         attr_reader :member
 
+        # @param [Class] primitive
+        # @param [Hash] options
+        # @option options [Definition] :member
         def initialize(primitive, options = {})
           @member = options.fetch(:member)
           super
         end
 
+        # @param [Object] input
+        # @param [Symbol] meth
+        # @return [Array]
         def call(input, meth = :call)
           input.map { |el| member.__send__(meth, el) }
         end
         alias_method :[], :call
 
-        def valid?(type)
-          super && type.all? { |el| member.valid?(el) }
+        # @param [Array, #all?] value
+        # @return [Boolean]
+        def valid?(value)
+          super && value.all? { |el| member.valid?(el) }
         end
 
+        # @param [Array, Object] input
+        # @param [#call] block
+        # @yieldparam [Failure] failure
+        # @yieldreturn [Result]
+        # @return [Result]
         def try(input, &block)
           if input.is_a?(::Array)
             result = call(input, :try)
