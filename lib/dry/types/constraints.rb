9bf72c88c085ad0f168f55d25eac6792014b8509
Alex Semyonov <alex@semyonov.us>
2016-11-13 00:34:53 +0300

    Document Dry::Types Constraints
    Signed-off-by: Alex Semyonov <alex@semyonov.us>

--- lib/dry/types/constraints.rb
+++ lib/dry/types/constraints.rb
@@ -4,12 +4,15 @@ require 'dry/logic/rule/predicate'
 
 module Dry
   module Types
+    # @param [Hash] options
+    # @return [Dry::Logic::Rule]
     def self.Rule(options)
       rule_compiler.(
         options.map { |key, val| Logic::Rule::Predicate.new(Logic::Predicates[:"#{key}?"]).curry(val).to_ast }
       ).reduce(:and)
     end
 
+    # @return [Dry::Logic::RuleCompiler]
     def self.rule_compiler
       @rule_compiler ||= Logic::RuleCompiler.new(Logic::Predicates)
     end
