5b57399b463d6dd36ef0c1c94ccfac9ac91f16e7
Alex Semyonov <alex@semyonov.us>
2016-11-12 22:54:23 +0300

    Document Dry::Types::Definition
    Signed-off-by: Alex Semyonov <alex@semyonov.us>

--- lib/dry/types/definition.rb
+++ lib/dry/types/definition.rb
@@ -9,10 +9,14 @@ module Dry
       include Options
       include Builder
 
+      # @return [Hash]
       attr_reader :options
 
+      # @return [Class]
       attr_reader :primitive
 
+      # @param [Class] primitive
+      # @return [Definition]
       def self.[](primitive)
         if primitive == ::Array
           Types::Array
@@ -23,29 +27,41 @@ module Dry
         end
       end
 
+      # @param [Class] primitive
+      # @param [Hash] options
       def initialize(primitive, options = {})
         super
         @primitive = primitive
         freeze
       end
 
+      # @return [String]
       def name
         primitive.name
       end
 
+      # @return [false]
       def default?
         false
       end
 
+      # @return [false]
       def constrained?
         false
       end
 
+      # @param [Object] input
+      # @return [Object]
       def call(input)
         input
       end
       alias_method :[], :call
 
+      # @param [Object] input
+      # @param [#call] block
+      # @yieldparam [Failure] failure
+      # @yieldreturn [Result]
+      # @return [Result]
       def try(input, &block)
         if valid?(input)
           success(input)
@@ -55,18 +71,29 @@ module Dry
         end
       end
 
+      # @param (see Dry::Types::Success#initialize)
+      # @return [Success]
       def success(input)
         Result::Success.new(input)
       end
 
+
+      # @param (see Failure#initialize)
+      # @return [Failure]
       def failure(input, error)
         Result::Failure.new(input, error)
       end
 
+      # @param [Object] klass class of the result instance
+      # @param [Array] args arguments for the +klass#initialize+ method
+      # @return [Object] new instance of the given +klass+
       def result(klass, *args)
         klass.new(*args)
       end
 
+      # Checks whether value is of a #primitive class
+      # @param [Object] value
+      # @return [Boolean]
       def primitive?(value)
         value.is_a?(primitive)
       end
