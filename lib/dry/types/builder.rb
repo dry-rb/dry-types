06501a2fcb66795308421775bb1e17431c82ff90
Alex Semyonov <alex@semyonov.us>
2016-11-12 23:22:25 +0300

    Document Dry::Types::Builder
    Signed-off-by: Alex Semyonov <alex@semyonov.us>

--- lib/dry/types/builder.rb
+++ lib/dry/types/builder.rb
@@ -1,23 +1,35 @@
 module Dry
   module Types
     module Builder
+      include Dry::Core::Constants
+
+      # @return [Constrained]
       def constrained_type
         Constrained
       end
 
+      # @param [Definition] other
+      # @return [Sum, Sum::Constrained]
       def |(other)
         klass = constrained? && other.constrained? ? Sum::Constrained : Sum
         klass.new(self, other)
       end
 
+      # @return [Sum]
       def optional
         Types['strict.nil'] | self
       end
 
+      # @param [Hash] options constraining rule (see {Types.Rule})
+      # @return [Constrained]
       def constrained(options)
         constrained_type.new(self, rule: Types.Rule(options))
       end
 
+      # @param [Object] input
+      # @param [#call] block
+      # @raise [ConstraintError]
+      # @return [Default]
       def default(input = Undefined, &block)
         value = input == Undefined ? block : input
 
@@ -28,14 +40,21 @@ module Dry
         end
       end
 
+      # @param [Array] values
+      # @return [Enum]
       def enum(*values)
         Enum.new(constrained(included_in: values), values: values)
       end
 
+      # @return [Safe]
       def safe
         Safe.new(self)
       end
 
+      # @param [#call] constructor
+      # @param [Hash] options
+      # @param [#call] block
+      # @return [Constructor]
       def constructor(constructor = nil, **options, &block)
         Constructor.new(with(options), fn: constructor || block)
       end
