444ec4df25f5f462a4d63d44f6c4cc0aaca88c40
GustavoCaso <gustavocaso@gmail.com>
2017-03-03 09:30:09 +0100

    Order matters for rspec ?

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -37,6 +37,10 @@ require_relative '../lib/spec/dry/types'
 Undefined = Dry::Core::Constants::Undefined
 
 RSpec.configure do |config|
+  config.before(:example, :maybe) do
+    Dry::Types.load_extensions(:maybe)
+  end
+
   config.before do
     @types = Dry::Types.container._container.keys
 
@@ -57,10 +61,6 @@ RSpec.configure do |config|
   end
 
   config.order = 'random'
-
-  config.before(:example, :maybe) do
-    Dry::Types.load_extensions(:maybe)
-  end
 end
 
 srand RSpec.configuration.seed
