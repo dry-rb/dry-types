b8838f1c39c70e82272f5495c287ee10f4b99143
Nikita Shilnikov <fg@flashgordon.ru>
2018-04-24 15:31:57 +0300

    Add form methods to Compiler for backward compat

--- lib/dry/types/compat/form_types.rb
+++ lib/dry/types/compat/form_types.rb
@@ -10,5 +10,17 @@ module Dry
     end
 
     register('form.int', self['params.integer'])
+
+    class Compiler
+      def visit_form_hash(node)
+        schema, meta = node
+        merge_with('params.hash', :symbolized, schema).meta(meta)
+      end
+
+      def visit_form_array(node)
+        member, meta = node
+        registry['params.array'].of(visit(member)).meta(meta)
+      end
+    end
   end
 end
