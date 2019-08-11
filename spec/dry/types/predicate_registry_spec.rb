e808f2ee8e2d7f3b5df832b363714c80fd4da8c9
Nikita Shilnikov <fg@flashgordon.ru>
2019-08-11 11:42:50 +0300

    Add predicate inferrer
    It's a port from dry-schema

---
+++ spec/dry/types/predicate_registry_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'dry/types/predicate_registry'
+
+RSpec.describe Dry::Types::PredicateRegistry do
+  subject(:predicate_registry) { Dry::Types::PredicateRegistry.new }
+
+  describe '#[]' do
+    it 'gives access to built-in predicates' do
+      expect(predicate_registry[:filled?].('sutin')).to be(true)
+    end
+  end
+
+  describe '#key?' do
+    it 'checks whether a predicate is registered' do
+      expect(predicate_registry.key?(:respond_to?)).to be(true)
+      expect(predicate_registry.key?(:nope?)).to be(false)
+    end
+  end
+end
