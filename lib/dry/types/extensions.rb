b68d52e8391d8c79b80f01e4a8f0ced97dde8b58
Svyatoslav Kryukov <s.g.kryukov@yandex.ru>
2019-09-15 14:17:00 +0300

    Add monads extension

--- lib/dry/types/extensions.rb
+++ lib/dry/types/extensions.rb
@@ -3,3 +3,7 @@
 Dry::Types.register_extension(:maybe) do
   require 'dry/types/extensions/maybe'
 end
+
+Dry::Types.register_extension(:monads) do
+  require 'dry/types/extensions/monads'
+end
---
+++ spec/extensions/monads/result_spec.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+RSpec.describe Dry::Types::Result do
+  before { Dry::Types.load_extensions(:monads) }
+
+  let(:type) { Dry::Types['strict.string'] }
+
+  let(:result) { type.try(input) }
+
+  context 'interface' do
+    let(:input) { {} }
+
+    it 'responds to #to_monad' do
+      expect(result).to respond_to(:to_monad)
+    end
+  end
+
+  context 'with valid input' do
+    let(:input) { 'Jane' }
+
+    describe '#to_monad' do
+      it 'wraps Result with Success' do
+        monad = result.to_monad
+
+        expect(monad).to be_a Dry::Monads::Result
+        expect(monad).to be_success
+        expect(monad.value!).to eq(result.input)
+      end
+    end
+  end
+
+  context 'with invalid input' do
+    let(:input) { nil }
+
+    describe '#to_monad' do
+      it 'wraps Result with Failure' do
+        monad = result.to_monad
+
+        expect(monad).to be_a Dry::Monads::Result
+        expect(monad).to be_failure
+        expect(monad.failure).to eq([result.error, result.input])
+      end
+    end
+  end
+end
