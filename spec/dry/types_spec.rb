83026f841f1bd12c434dee54f7147ca3fdf3ac38
Piotr Solnica <piotr.solnica@gmail.com>
2020-12-12 08:24:31 +0100

    rubocop -a --only Style/StringLiterals

--- spec/dry/types_spec.rb
+++ spec/dry/types_spec.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 RSpec.describe Dry::Types do
-  describe '.register' do
-    it 'registers a new type constructor' do
+  describe ".register" do
+    it "registers a new type constructor" do
       module Test
         module FlatArray
           def self.constructor(input)
@@ -19,7 +19,7 @@ RSpec.describe Dry::Types do
     end
   end
 
-  describe '.[]' do
+  describe ".[]" do
     before do
       module Test
         class Foo < Dry::Types::Nominal
@@ -33,25 +33,25 @@ RSpec.describe Dry::Types do
     let(:unregistered_type) { Test::Foo }
 
     it 'returns registered type for "string"' do
-      expect(Dry::Types['nominal.string']).to be_a(Dry::Types::Nominal)
-      expect(Dry::Types['nominal.string'].name).to eql('String')
+      expect(Dry::Types["nominal.string"]).to be_a(Dry::Types::Nominal)
+      expect(Dry::Types["nominal.string"].name).to eql("String")
     end
 
-    it 'caches dynamically built types' do
-      expect(Dry::Types['array<string>']).to be(Dry::Types['array<string>'])
+    it "caches dynamically built types" do
+      expect(Dry::Types["array<string>"]).to be(Dry::Types["array<string>"])
     end
 
-    it 'returns unregistered types back' do
+    it "returns unregistered types back" do
       expect(Dry::Types[unregistered_type]).to be(unregistered_type)
     end
 
-    it 'has strict types as default in optional namespace' do
-      expect(Dry::Types['optional.string']).to eql(Dry::Types['string'].optional)
+    it "has strict types as default in optional namespace" do
+      expect(Dry::Types["optional.string"]).to eql(Dry::Types["string"].optional)
     end
   end
 
-  describe 'missing constant' do
-    it 'raises a nice error when a constant like Coercible or Strict is missing' do
+  describe "missing constant" do
+    it "raises a nice error when a constant like Coercible or Strict is missing" do
       expect {
         Dry::Types::Strict::String
       }.to raise_error(NameError, /dry-types does not define constants for default types/)
